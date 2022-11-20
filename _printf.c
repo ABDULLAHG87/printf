48e2e4164de31b10769dc283893de4fcbd82d582
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-20 06:39:21 -0800

    Task 0 committed

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
+
 /**
  * _printf - ...
  * @format: Input String
@@ -7,13 +9,20 @@
  * Return: int
  */
 
+int _formatspec(va_list args, const char *str);
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	va_start(args, format);
 	const char *str;
 	int i = 0;
 	int count = 0;
+
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
 
 	for (str = format; *str != '\0'; str++)
 	{
@@ -27,26 +36,40 @@ int _printf(const char *format, ...)
 		if (*str == '%')
 		{
 			str++;
-			if (*str == 'c')
-			{
-				int c = va_arg(args, int);
-				_putchar(c);
-			}
-			else if (*str == 's')
-			{
-				char *s = va_arg(args, char *);
-				while(*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-				}
-			}
-			else if (*str == '%')
-			{
-				_putchar(37);
-			}
+			_formatspec(args, str);
 		}
 	}
 	va_end(args);
 	return (count);
+}
+
+/**
+ * _formatspec - ...
+ * @str: String
+ * Return: int
+ */
+
+int _formatspec(va_list args, const char *str)
+{
+	if (*str == 'c')
+	{
+		int c = va_arg(args, int);
+
+		_putchar(c);
+	}
+	else if (*str == 's')
+	{
+		char *s = va_arg(args, char *);
+
+		while (*s != '\0')
+		{
+			_putchar(*s);
+			s++;
+		}
+	}
+	else if (*str == '%')
+	{
+		_putchar(37);
+	}
+	return (0);
 }
